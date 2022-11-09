edec5521620309b9af76065a5041d4251cf4d41d
octaviopc <operezcenteno@deloitte.com>
2022-11-08 23:27:21 -0600

    Declarations in AutoMapper

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Back_End.Dtos.User;
+
+namespace Back_End
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User,GetUserDto>();
+            CreateMap<AddUserDto,User>();
+        }
+        
+    }        
+}
