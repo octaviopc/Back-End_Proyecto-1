b6af3ce72f5984150ab0f241950a9f508bb46fbe
octaviopc <operezcenteno@deloitte.com>
2022-11-08 23:01:49 -0600

    Creation of firsts Dtos

---
+++ Dtos/User/GetUserDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back_End.Dtos.User
+{
+    public class GetUserDto
+    {
+        
+    }
+}
